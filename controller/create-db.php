3d07a2d1ff3bd2bcdb45f958c24e3a654f1fb688
Anderson Nguyen <andersonnguyen126@gmail.com>
2014-10-28 12:18:31 -0700

    Added coments

--- controller/create-db.php
+++ controller/create-db.php
@@ -3,25 +3,28 @@
 
 	$connection = new mysqli($host, $username, $password);
 
-	//* If the connection fails state the the error.
+	//*  Checks for an error. If the connection fails it is told to DIE.
 	if ($connection->connect_error) {
 		die("Error: " . $connection->connect_error);
 	}
 
 	$exists = $connection->select_db($database);
-
+	//* If connection exists it'll run query.
 	if (!$exists) {
+		//* Query asks if there is a connection to the database. If not it'll create a database.
 		$query = $connection->query("CREATE DATABASE $database");
 
 		if ($query) {
+			//* Echoes text if a database is created.
 			echo "Successfully created database: " . $database;
 		}
 	}
 
 	else {
+		//* Echoes text if database already exists.
 		echo "Database already exists";
 	}
-
+	//* Closes connection.
 	$connection->close();
 
 ?>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 </head>
 <body>
 	<?php 
+		//* Will check if the file is included. If so it will not require it again.
 		require_once(__DIR__ . "/controller/create-db.php");
 	?>
 </body>
