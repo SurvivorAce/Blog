97ebd8d0f5dc92efbccf025c3713884e4f772f60
Anderson Nguyen <andersonnguyen126@gmail.com>
2014-11-03 13:25:24 -0800

    header and footer stuff

--- controller/create-db.php
+++ controller/create-db.php
@@ -5,7 +5,7 @@
 
 	//*  Checks for an error. If the connection fails it is told to DIE.
 	if ($connection->connect_error) {
-		die("Error: " . $connection->connect_error);
+		die("<p>Error: " . $connection->connect_error . "</p>");
 	}
 
 	$exists = $connection->select_db($database);
@@ -16,21 +16,29 @@
 
 		if ($query) {
 			//* Echoes text if a database is created.
-			echo "Successfully created database: " . $database;
+			echo "<p>Successfully created database: " . $database . "</p>";
 		}
 	}
 
 	else {
 		//* Echoes text if database already exists.
-		echo "Database already exists";
+		echo "<p>Database already exists</p>";
 	}
 
 	//* Creates a Table.							
 	$query = $connection->query("CREATE TABLE posts ("
-			 . "id int(11) NOT NULL AUTO_INCREMENT"  //* ID. 11 values inside the integer. ID can't be null. Auto increments the ID numbers.
-			 . "title varchar(255) NOT NULL"	//* Title. Atleast 255 characters for a title. Title can't be null.
-			 . "post text NOT NULL"		//* Post. Will contain text. Can't be null.					
+			 . "id int(11) NOT NULL AUTO_INCREMENT,"  //* ID. 11 values inside the integer. ID can't be null. Auto increments the ID numbers.
+			 . "title varchar(255) NOT NULL,"	//* Title. Atleast 255 characters for a title. Title can't be null.
+			 . "post text NOT NULL,"		//* Post. Will contain text. Can't be null.					
 			 . "PRIMARY KEY (id))");	//* Tells the table that the primary key is id.
+
+	if($query) {
+		echo "<p>Successfully created table: posts</p>";	// Informs you that it succesfully created table: posts.
+	}
+
+	else {
+		echo "<p>$connection->error</p>";	// Informs you know that table: posts already exists.
+	}
 
 	//* Closes connection.
 	$connection->close();
--- index.php
+++ index.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>BLOG</title>
-</head>
-<body>
+
 	<?php 
-		//* Will check if the file is included. If so it will not require it again.
+		//* Will check if the file is included. If so it will not require it again. 
+		require_once(__DIR__ . "/view/header.php");
 		require_once(__DIR__ . "/controller/create-db.php");
+		require_once(__DIR__ . "/view/footer.php");
 	?>
-</body>
-</html>
 
 
 
