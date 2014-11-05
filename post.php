ed7a5bc88f5f3efa2b74e1bf439a098ab59b2c1a
Anderson Nguyen <andersonnguyen126@gmail.com>
2014-11-05 12:16:59 -0800

    Added css and form.php

---
+++ post.php
@@ -0,0 +1,6 @@
+<?php
+	//* Will check if the file is included. If so it will not require it again. 
+	require_once(__DIR__ . "/view/header.php");
+	require_once(__DIR__ . "/view/form.php");
+	require_once(__DIR__ . "/view/footer.php");
+?>
---
+++ view/form.php
@@ -0,0 +1,13 @@
+<h1>Create Blog Post</h1>
+
+<form>
+	<div>
+		<label for="title">Title: </label> <!--Sets the lable "title" for your post -->
+		<input type="text" name="title"/>	<!--Makes a chat box thing for you to lable your title -->
+	</div>
+
+	<div>
+		<label for="post">Post: </label> <!--Sets a lable "post" for your post-->
+		<textarea name="post"> </textarea> <!--Adds chat box thing for your post WHICH ALSO EXPANDS! -->
+	</div>
+</form>
