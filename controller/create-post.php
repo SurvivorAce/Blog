10fe1a84efd31e616fb198f36d7335041fb35194
Anderson Nguyen <andersonnguyen126@gmail.com>
2014-11-13 12:22:43 -0800

    Completed video 24.

--- controller/create-post.php
+++ controller/create-post.php
@@ -6,9 +6,15 @@
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING); //* Recieves Title Input
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING); //* Recieves Post Input
 
+	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'"); //* Inserts new records into a table
 
-	echo "<p>Title: $title</p>"; //* Echoes Title Input
-	echo "<p>Post: $post</p>"; //* Echoes Post Input
+	if($query) {
+		echo "<p>Successfully inserted post: $title</p>"; //* If query is working, it will echo that it successfully inserted the post and the name of the title
+	}
+
+	else{
+		echo "<p>$connection->error</p>"; //* If query ISN'T working then it will echo the error
+	}
 
 	$connection->close(); //*Closes Connection
 ?>
