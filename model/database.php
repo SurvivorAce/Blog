9d4f8059ffd295b99653054ac7162aa94271a3f8
Anderson Nguyen <andersonnguyen126@gmail.com>
2014-11-17 13:21:37 -0800

    videos 27 and 28

--- model/database.php
+++ model/database.php
@@ -1,12 +1,19 @@
 <?php
 	//* Classes are used to be called in an instance of an object. 
 	class Database {
+		//*Global variables
 		private $connection;
 		private $host;
 		private $username;
 		private $password;
 		private $database;
-	}
-	
 
+		//* function __construct is used to define a constructor
+		public function __construct($host, $username, $path, $database) { //*Local variables 
+			$this->host = $host; 
+			$this->username = $username;
+			$this->password = $password;
+			$this->database = $database;
+		}
+	}
 ?>
